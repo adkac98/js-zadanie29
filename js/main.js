b16602075ce1c2ed611c3aaed8034932017b103f
adam kacki <adam.kacki@icloud.com>
2021-02-15 19:51:00 +0100

    ropoczęcie zadania

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let firstNumner = 3
+let secondNumber = 5
+function sum(firstNumner, secondNumber){
+    let result = firstNumner + secondNumber;
+    return result
+}
+console.log(sum(firstNumner,secondNumber))
